08a34ae3d0fa66245913acff83ddbf7e133fa027
Михаил Падалка <padalka@mac-0001.local>
2016-02-25 16:52:45 +0200

    first commit

---
+++ Hipnosister/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hipnosister
+//
+//  Created by Михаил Падалка on 2/25/16.
+//  Copyright © 2016 Михаил Падалка. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
